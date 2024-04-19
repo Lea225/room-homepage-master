00175098285a76940dc0cf83d921ca85992672cd
Lea225 <johndoe@example.com>
2024-04-19 17:04:54 +0100

    third commit

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Récupérer les éléments nécessaires
-  const hamburgerIcon = document.getElementById('icon-hamburger');
+  const hamburgerIcons = document.querySelectorAll('.icon-hamburger');
   const closeIcon = document.getElementById('icon-close');
   const mobileMenu = document.getElementById('mobileMenu');
   const overlay = document.getElementById('overlay');
-  const leftSlider = document.querySelector('.left-slider');
-  const rightSlider = document.querySelector('.right-slider');
-  const section1 = document.querySelector('.section-1');
-  const section2 = document.querySelector('.section-2');
-  const section3 = document.querySelector('.section-3');
-  const section4 = document.querySelector('.section-4');
+  const leftSliders = document.querySelectorAll('.left-slider');
+  const rightSliders = document.querySelectorAll('.right-slider');
+  const sections = document.querySelectorAll('.container-1, .container-2, .container-3');
+
+  let currentSectionIndex = 0; // Index de la section actuellement affichée
 
   // Écouter le clic sur l'icône hamburger pour afficher le menu mobile et l'overlay
-  hamburgerIcon.addEventListener('click', function() {
-      mobileMenu.classList.add('show');
-      overlay.style.display = 'block';
+  hamburgerIcons.forEach((icon) => {
+      icon.addEventListener('click', function() {
+          mobileMenu.classList.add('show');
+          overlay.style.display = 'block';
+      });
   });
 
   // Écouter le clic sur l'icône close pour masquer le menu mobile et l'overlay
@@ -23,27 +24,32 @@ document.addEventListener('DOMContentLoaded', () => {
       overlay.style.display = 'none';
   });
 
+  function showSection(index) {
+      sections.forEach((section, i) => {
+          if (i === index) {
+              section.style.display = 'grid'; // Modification ici pour afficher en flex
+          } else {
+              section.style.display = 'none';
+          }
+      });
+  }
+
   // Événement pour le clic sur la div left-slider
-  leftSlider.addEventListener('click', function() {
-      if (window.innerWidth <= 767) {
-          section1.style.backgroundImage = 'url(images/mobile-image-hero-2.jpg)';
-      } else {
-          section1.style.backgroundImage = 'url(images/desktop-image-hero-2.jpg)';
-      }
-      section2.style.display = 'none';
-      section3.style.display = 'block';
-      section4.style.display = 'none';
+  leftSliders.forEach((slider) => {
+      slider.addEventListener('click', function() {
+          currentSectionIndex = (currentSectionIndex - 1 + sections.length) % sections.length;
+          showSection(currentSectionIndex);
+      });
   });
 
   // Événement pour le clic sur la div right-slider
-  rightSlider.addEventListener('click', function() {
-      if (window.innerWidth <= 767) {
-          section1.style.backgroundImage = 'url(images/mobile-image-hero-3.jpg)';
-      } else {
-          section1.style.backgroundImage = 'url(images/desktop-image-hero-3.jpg)';
-      }
-      section2.style.display = 'none';
-      section3.style.display = 'none';
-      section4.style.display = 'block';
+  rightSliders.forEach((slider) => {
+      slider.addEventListener('click', function() {
+          currentSectionIndex = (currentSectionIndex + 1) % sections.length;
+          showSection(currentSectionIndex);
+      });
   });
+
+  // Afficher la première section au chargement de la page
+  showSection(currentSectionIndex);
 });
